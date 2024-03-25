2a4e0059edcf2489851ce8ca192317f8ad359468
Septillion24 <noahgrosh@gmail.com>
2024-03-25 13:01:31 -0400

    make base files for project

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, abort, redirect, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/test1')
+def test1():
+    return render_template("index.html", test1=True)
+
+@app.route('/test2')
+def test2():
+    return render_template("index.html", test2=True)
+
+@app.route('/test3')
+def test3():
+    return render_template("index.html", test3=True)
