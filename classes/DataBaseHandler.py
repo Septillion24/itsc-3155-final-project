2d5e3b3401744ab6e93bca237c8ad44b1123346a
James Rosa <jrosa6@charlotte.edu>
2024-04-10 11:52:19 -0400

    changes for pq

--- classes/DataBaseHandler.py
+++ classes/DataBaseHandler.py
@@ -1,5 +1,6 @@
 from classes.User import User
 import psycopg
+from db_secrets import db_info
 
 class DataBaseHandler:
     instance = None
@@ -16,19 +17,26 @@ class DataBaseHandler:
     # these methods are not final, change them if you will
     def getPosts(self):
         with psycopg.connect(
-        conninfo = "dbname=postgres user=postgres password=changeme host=localhost"
+        conninfo = db_info()
         ) as conn:
             with conn.cursor() as cur:
                 cur.execute('SELECT * FROM posts')
                 rows = cur.fetchall()
                 return
             
-    def createPost(self, user:User, title:str, content:str):
-        pass
+    def createPost(self, user:User, content:str, commentIDs:list):
+        with psycopg.connect(
+        conninfo = db_info()
+        ) as conn:
+            with conn.cursor() as cur:
+                # double check commentIDs value passing
+                cur.execute(F'''INSERT INTO posts (username, content, commentIDs) VALUES
+                    ('{user.name}', '{content}', '{commentIDs}')''')
+                
 
     def getPostByID(self, postID: int):
         with psycopg.connect(
-        conninfo = "dbname=postgres user=postgres password=changeme host=localhost"
+        conninfo = db_info()
         ) as conn:
             with conn.cursor() as cur:
                 cur.execute(f'SELECT * FROM posts WHERE posts.postID = {postID}')
@@ -36,7 +44,7 @@ class DataBaseHandler:
                 return rows
     def getUsers(self):
         with psycopg.connect(
-        conninfo = "dbname=postgres user=postgres password=changeme host=localhost"
+        conninfo = db_info()
         ) as conn:
             with conn.cursor() as cur:
                 cur.execute('SELECT * FROM users')
@@ -48,7 +56,7 @@ class DataBaseHandler:
 
     def getUserByID(self, userID: str):
         with psycopg.connect(
-        conninfo = "dbname=postgres user=postgres password=changeme host=localhost"
+        conninfo = db_info()
         ) as conn:
             with conn.cursor() as cur:
                 cur.execute(f'select * from users where users.username = \'{userID}\'')
