4a8c62389cb00a099bfd923a6aab64a0f98991b6
septillion <noahgrosh@gmail.com>
2024-04-24 16:39:59 -0400

    load posts works

--- static/scripts/loadPosts.js
+++ static/scripts/loadPosts.js
@@ -2,15 +2,17 @@ function populatePosts() {
     fetch("/forum/getposts?numPosts=10")
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
+            console.log(data);
             const container = document.getElementById("postsContainer");
             data.forEach((item) => {
                 const postDiv = document.createElement("div");
-                postDiv.innerHTML = `<strong>Post ID:</strong> ${item.id} <br> <strong>Content:</strong> ${item.content}`;
+                postDiv.innerHTML = `
+                
+                `; // off-brand jsx
                 container.appendChild(postDiv);
             });
         })
         .catch((error) => console.error("Error fetching posts:", error));
 }
 
-console.log(populatePosts())
+console.log(populatePosts());
