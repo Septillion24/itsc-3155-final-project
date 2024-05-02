3e9dcbd6c40e10bfab7f6a9ba39feb5f3ce311e2
septillion <noahgrosh@gmail.com>
2024-05-02 17:18:32 -0400

    this might fix editing

--- app.py
+++ app.py
@@ -176,10 +176,8 @@ def getUserByID(userID:int):
     user = db.getUserByID(userID)
     logged_in = session.get('authenticated', False)
     user_id = session.get('user_id')
-    if user_id is not None: 
-        owner = userID == user_id
-    else:
-        owner = False 
+    owner = userID == user_id
+
     return render_template("user.html", user=user, logged_in=logged_in, owner=owner)
 
 @app.get("/user/<int:userID>/posts")
