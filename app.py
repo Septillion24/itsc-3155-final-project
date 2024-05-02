e48c78877d384dd34ab4ef5e1ba14dfd532db291
septillion <noahgrosh@gmail.com>
2024-05-01 21:08:51 -0400

    i forgot what i fixed

--- app.py
+++ app.py
@@ -106,10 +106,12 @@ def getPostsForForumPage():
 
 @app.get('/forum/post/<int:post_id>')
 def getPostFromID(post_id):
-    discussion = db.getPostByID(post_id)
-    if discussion is None:
+    post = db.getPostByID(post_id)
+    if post is None:
         abort(404)
-    return discussion, 200
+
+    logged_in = session.get('authenticated', False)
+    return render_template("singlePost.html", post=post, logged_in=logged_in)
 
 @app.post("/forum/makepost")
 def createPost():
